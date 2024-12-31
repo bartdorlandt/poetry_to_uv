d112403835f4340d3f9d2e0a8df5c0e101a805eb
Bart Dorlandt <bart@bamweb.nl>
2024-12-31 21:49:30 +0100

    Fixed tests, cleanup

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ It has a dry-run flag, to have a temporary file to validate the output. When not
 
     uv run poetry_to_uv.py <path to file> [-n]
 
-You may need to make some manual changes, probably around the following:
+You may need to make some manual changes. Certain layout things are best done with [Even better toml](https://marketplace.visualstudio.com/items?itemName=tamasfe.even-better-toml) in VSCode. Just open the newly generated toml file and save. It will format the file according to the toml specification.
 
-* tool sections that use the multiline syntax in the exclude assignments (ruff, mypy, black, ...)
+## Notes
+* If you were using the poetry build-system, it will be replaced by hatchling.
 * if you had optional dev groups, the dev group libraries will be used, the optional flag is removed
 
 # Using as a tool
--- tests/test_poetry_to_uv.py
+++ tests/test_poetry_to_uv.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import pytest
-import toml
+import tomlkit
 
 import poetry_to_uv
 
@@ -13,8 +13,46 @@ def read_poetry_toml_as_text():
 
 
 @pytest.fixture
+def org_toml():
+    return {
+        "tool": {
+            "poetry": {
+                "dependencies": {
+                    "pytest": "*",
+                    "pytest-cov": "*",
+                    "jira": "^3.8.0",
+                },
+                "group": {
+                    "dev": {
+                        "dependencies": {
+                            "mypy": "^1.0.1",
+                        }
+                    }
+                },
+            }
+        }
+    }
+
+
+@pytest.fixture
+def org_toml_optional():
+    return {
+        "tool": {
+            "poetry": {
+                "dependencies": {
+                    "pytest": "*",
+                    "pytest-cov": "*",
+                    "jira": {"version": "^3.8.0", "optional": True},
+                },
+                "extras": {"JIRA": ["jira"]},
+            }
+        }
+    }
+
+
+@pytest.fixture
 def read_poetry_toml_as_object(read_poetry_toml_as_text):
-    return toml.loads(read_poetry_toml_as_text)
+    return tomlkit.loads(read_poetry_toml_as_text)
 
 
 @pytest.mark.parametrize(
@@ -28,8 +66,8 @@ def read_poetry_toml_as_object(read_poetry_toml_as_text):
 )
 def test_authors_maintainers(key, name, email):
     authors = [f"{name} <{email}>"]
-    in_dict = {"project": {key: authors}}
-    expected = {"project": {key: f'[{{name = "{name}", email = "{email}"}}]'}}
+    in_dict = {key: authors}
+    expected = {key: [{"name": name, "email": email}]}
     poetry_to_uv.authors_maintainers(in_dict)
     assert in_dict == expected
 
@@ -39,26 +77,33 @@ def test_authors_maintainers(key, name, email):
     [
         (
             ["First Last <first@domain2.nl>", "another <email@domain.nl>"],
-            '[{name = "First Last", email = "first@domain2.nl"}, {name = "another", email = "email@domain.nl"}]',
+            [
+                {"name": "First Last", "email": "first@domain2.nl"},
+                {"name": "another", "email": "email@domain.nl"},
+            ],
         ),
         (
             ["First Last", "<email@domain.nl>"],
-            '[{name = "First Last"}, {email = "email@domain.nl"}]',
+            [{"name": "First Last"}, {"email": "email@domain.nl"}],
         ),
         (
             ["First Last <first@domain2.nl>", "<email@domain.nl>", "First Last"],
-            '[{name = "First Last", email = "first@domain2.nl"}, {email = "email@domain.nl"}, {name = "First Last"}]',
+            [
+                {"name": "First Last", "email": "first@domain2.nl"},
+                {"email": "email@domain.nl"},
+                {"name": "First Last"},
+            ],
         ),
     ],
 )
 def test_multiple_authors(authors, author_string):
-    in_dict = {"project": {"authors": authors}}
-    expected = {"project": {"authors": author_string}}
+    in_dict = {"authors": authors}
+    expected = {"authors": author_string}
     poetry_to_uv.authors_maintainers(in_dict)
     assert in_dict == expected
 
 
-def test_dependencies():
+def test_dependencies(org_toml):
     in_dict = {
         "project": {
             "dependencies": {
@@ -69,19 +114,17 @@ def test_dependencies():
         }
     }
     expected = {"project": {"dependencies": ["pytest", "pytest-cov", "jira>=3.8.0"]}}
-    poetry_to_uv.dependencies(in_dict)
+    poetry_to_uv.dependencies(in_dict, org_toml)
     assert in_dict == expected
 
 
-def test_optional_dependencies():
+def test_optional_dependencies(org_toml_optional):
     in_dict = {
         "project": {
             "dependencies": {
                 "pytest": "*",
                 "pytest-cov": "*",
-                "jira": {"version": "^3.8.0", "optional": True},
             },
-            "extras": {"JIRA": ["jira"]},
         }
     }
     expected = {
@@ -90,159 +133,36 @@ def test_optional_dependencies():
             "optional-dependencies": {"JIRA": ["jira>=3.8.0"]},
         }
     }
-    poetry_to_uv.dependencies(in_dict)
+    poetry_to_uv.dependencies(in_dict, org_toml_optional)
     assert in_dict == expected
 
 
-def test_dev_dependencies():
-    in_dict = {
-        "project": {
-            "group": {
-                "dev": {
-                    "dependencies": {
-                        "pytest": "*",
-                        "pytest-cov": "*",
-                        "jira": "^3.8.0",
-                    }
-                }
-            }
-        }
-    }
+def test_dev_dependencies(org_toml):
+    in_dict = {"project": {}}
     expected = {
         "project": {},
-        "dependency-groups": {"dev": ["pytest", "pytest-cov", "jira>=3.8.0"]},
+        "dependency-groups": {"dev": ["mypy>=1.0.1"]},
     }
-    poetry_to_uv.dev_dependencies(in_dict)
+    poetry_to_uv.dev_dependencies(in_dict, org_toml)
     assert in_dict == expected
 
 
-def test_modify_authors_line():
-    in_txt = """[project]
-name = "someproject"
-version = "0.1.0"
-description = "A project"
-authors = "[{ name = \"First Last\", email = \"first@domain.nl\" }]"
-license = "LICENSE"
-readme = "README.md"
-requires-python = ">=3.12"
-dependencies = [
-    "jira>=3.8.0",
-]
-"""
-    out_txt = """[project]
-name = "someproject"
-version = "0.1.0"
-description = "A project"
-authors = [{ name = "First Last", email = "first@domain.nl" }]
-license = "LICENSE"
-readme = "README.md"
-requires-python = ">=3.12"
-dependencies = [
-    "jira>=3.8.0",
-]
-"""
-    result = poetry_to_uv.modify_authors_maintainers_line(in_txt, "authors")
-    assert result == out_txt
-    assert (
-        poetry_to_uv.modify_authors_maintainers_line(result, "maintainers") == out_txt
-    )
-
-
-def test_modify_maintainers_line():
-    in_txt = """[project]
-name = "someproject"
-version = "0.1.0"
-description = "A project"
-maintainers = "[{ name = \"First Last\", email = \"first@domain.nl\" }]"
-license = "LICENSE"
-readme = "README.md"
-requires-python = ">=3.12"
-dependencies = [
-    "jira>=3.8.0",
-]
-"""
-    out_txt = """[project]
-name = "someproject"
-version = "0.1.0"
-description = "A project"
-maintainers = [{ name = "First Last", email = "first@domain.nl" }]
-license = "LICENSE"
-readme = "README.md"
-requires-python = ">=3.12"
-dependencies = [
-    "jira>=3.8.0",
-]
-"""
-    assert (
-        poetry_to_uv.modify_authors_maintainers_line(in_txt, "maintainers") == out_txt
-    )
-
-
-def test_modify_authors_and_maintainers_line():
-    in_txt = """[project]
-name = "someproject"
-version = "0.1.0"
-description = "A project"
-authors = "[{ name = \"First Last\", email = \"first@domain.nl\" }]"
-maintainers = "[{ name = \"First Last\", email = \"first@domain.nl\" }]"
-license = "LICENSE"
-readme = "README.md"
-requires-python = ">=3.12"
-dependencies = [
-    "jira>=3.8.0",
-]
-"""
-    out_txt = """[project]
-name = "someproject"
-version = "0.1.0"
-description = "A project"
-authors = [{ name = "First Last", email = "first@domain.nl" }]
-maintainers = [{ name = "First Last", email = "first@domain.nl" }]
-license = "LICENSE"
-readme = "README.md"
-requires-python = ">=3.12"
-dependencies = [
-    "jira>=3.8.0",
-]
-"""
-    result = poetry_to_uv.modify_authors_maintainers_line(in_txt, "authors")
-    assert (
-        poetry_to_uv.modify_authors_maintainers_line(result, "maintainers") == out_txt
-    )
-
-
-def test_modify_authors_and_maintainers_line_with_multiple_ids(
-    read_poetry_toml_as_object,
-):
-    uv_toml = {"tool": {}, "project": read_poetry_toml_as_object["tool"]["poetry"]}
-    poetry_to_uv.authors_maintainers(uv_toml)
-    in_txt = toml.dumps(uv_toml)
-    result = poetry_to_uv.modify_authors_maintainers_line(in_txt, "authors")
-    result = poetry_to_uv.modify_authors_maintainers_line(result, "maintainers")
-    obj = toml.loads(result)
-    assert_accounts = [
-        {"name": "another", "email": "email@domain.nl"},
-        {"email": "some@email.nl"},
-        {"name": "user"},
-    ]
-    # sourcery skip: no-loop-in-tests
-    for account in assert_accounts:
-        assert account in obj["project"]["authors"]
-        assert account in obj["project"]["maintainers"]
+{"project": {"license": "MIT"}}
+{"project": {"license": {"text": "MIT"}}}
 
 
 def test_project_license():
-    in_dict = {"project": {"license": "MIT"}}
-    expected = {"project": {"license": {"text": "MIT"}}}
+    in_dict = {"license": "MIT"}
+    expected = {"license": {"text": "MIT"}}
     poetry_to_uv.project_license(in_dict, Path())
     assert in_dict == expected
 
 
 def test_project_license_file(tmp_path):
     license_name = "license_file_name"
-    in_dict = {"project": {"license": license_name}}
+    in_dict = {"license": license_name}
     tmp_path.joinpath(license_name).touch()
-    expected = {"project": {"license": {"file": license_name}}}
+    expected = {"license": {"file": license_name}}
     poetry_to_uv.project_license(in_dict, tmp_path)
     assert in_dict == expected
 
@@ -253,9 +173,9 @@ def test_build_system(read_poetry_toml_as_object):
     }
     expected = {
         "build-system": {
-            "requires": ["poetry-core>=1.0.0"],
-            "build-backend": "poetry.core.masonry.api",
+            "requires": ["hatchling"],
+            "build-backend": "hatchling.build",
         }
     }
-    poetry_to_uv.blocks_as_is(in_dict, in_dict)
+    poetry_to_uv.build_system(in_dict, in_dict)
     assert in_dict == expected
